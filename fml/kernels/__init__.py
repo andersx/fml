3ad7d214a2e1af49b8dc96455dcede797002f309
Anders S. Christensen <andersbiceps@gmail.com>
2016-12-15 18:24:13 +0100

    Added ARAD kernel assembly//get_alpha

--- fml/kernels/__init__.py
+++ fml/kernels/__init__.py
@@ -7,4 +7,5 @@ Provides
 """
 
 from kernels import laplacian_kernel, gaussian_kernel, get_alpha, get_prediction, get_alpha_from_distance, get_prediction_from_distance, manhattan_distance
+from fkernels import fget_alpha_arad
 __all__ = ['kernels']
--- fml/math/__init__.py
+++ fml/math/__init__.py
@@ -10,5 +10,6 @@ from .distance import manhattan_distance, l2_distance, p_distance
 from .farad import molecular_arad_l2_distance
 from .farad import molecular_arad_l2_distance_all
 from .farad import atomic_arad_l2_distance
+from .farad import atomic_arad_l2_distance_all
 
 __all__ = ['cho_solve', "distance", "farad"]
